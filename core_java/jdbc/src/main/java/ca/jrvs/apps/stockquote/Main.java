4848d1f6163a89079219a287f309f31a427780ad
tehtko <moderndsnsam@gmail.com>
2023-10-18 19:27:27 -0400

    Create DatabaseConnectionManager

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/Main.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/Main.java
@@ -1,12 +1,35 @@
 package ca.jrvs.apps.stockquote;
 
 import ca.jrvs.apps.stockquote.dao.Quote;
+import ca.jrvs.apps.stockquote.dao.QuoteDAO;
 import ca.jrvs.apps.stockquote.dao.QuoteHttpHelper;
+import ca.jrvs.apps.stockquote.dao.DatabaseConnectionManager;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 public class Main {
     public static void main(String[] args) {
         QuoteHttpHelper quoteHttpHelper = new QuoteHttpHelper("YOUR_API_KEY");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "postgres",
+                "postgres", "password");
 
-        Quote quote = quoteHttpHelper.fetchQuoteInfo("AAPL");
+        try {
+            Connection connection = dcm.getConnection();
+            QuoteDAO<Quote, Integer> quoteDAO = new QuoteDAO<Quote, Integer>(connection);
+            /*
+            Quote quote = quoteHttpHelper.fetchQuoteInfo("AAPL");
+            quote.setTimestamp(Timestamp.from(Instant.now()));
+            quoteDAO.save(quote);
+             */
+            Quote quote = quoteDAO.findById(1).get();
+            System.out.println(quote.getSymbol());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
