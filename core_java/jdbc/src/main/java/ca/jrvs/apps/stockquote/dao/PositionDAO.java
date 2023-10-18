8a39818d5977c9dc7052d498d9f95951326d220b
tehtko <moderndsnsam@gmail.com>
2023-10-18 19:27:48 -0400

    Create PositionDAO

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/PositionDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/PositionDAO.java
@@ -1,4 +1,36 @@
 package ca.jrvs.apps.stockquote.dao;
 
-public class PositionDAO {
+import java.sql.Connection;
+import java.util.Optional;
+
+public class PositionDAO<Position, Integer> implements CrudDao<Position, Integer> {
+    protected final Connection connection;
+
+    public PositionDAO(Connection connection) {
+        this.connection = connection;
+    }
+    @Override
+    public Position save(Position entity) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Optional<Position> findById(Integer integer) throws IllegalArgumentException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Iterable<Position> findAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer integer) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
