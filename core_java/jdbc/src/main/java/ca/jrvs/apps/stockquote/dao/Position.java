de26de9c75f3ad2f6cc95b6393fda755b45c3a52
tehtko <moderndsnsam@gmail.com>
2023-10-18 17:32:10 -0400

    Init CrudDao

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/Position.java
@@ -0,0 +1,9 @@
+package ca.jrvs.apps.stockquote.dao;
+
+public class Position {
+    private int id;
+    private String symbol;
+    private int numOfShares;
+    private double valuePaid; //total amount paid for shares
+
+}
