9aab1f9140d2900b3d0fbad81611d043acd4d640
David Gordon <44374315+davidgordon12@users.noreply.github.com>
2023-10-19 12:05:11 -0400

    Update sum_of_array solutionOne

--- practice/sqljava_superhero_app/java/sum_of_array.java
+++ practice/sqljava_superhero_app/java/sum_of_array.java
@@ -7,49 +7,56 @@ import java.util.Scanner;
 public class sum_of_array {
   private static final int ARRAY_LENGTH = 100;
 
-  public static void solutionOne(int[] arr) {
-    System.out.print("Enter target number: ");
+    public static void solutionOne(int[] arr) {
+    int points = 0;
     Scanner scanner = new Scanner(System.in);
-    int target = scanner.nextInt();
 
-    /*
-     * Search the array to try and find n
-     * The input array starts out sorted, but will get unsorted as the game goes on,
-     * so binary search is out the window.
-     * Linear search: O(n)
-     */
-    for (int i = 0; i < arr.length; i++) {
-      // TRUE: If we find it, swap the first and last elements
-      if (arr[i] == target) {
-        int temp = arr[0];
-        arr[0] = arr[ARRAY_LENGTH - 1];
-        arr[ARRAY_LENGTH - 1] = temp;
+    while(points <= 10) {
+      System.out.print("Enter target number: ");
+      int target = scanner.nextInt();
+
+      /*
+       * Search the array to try and find n
+       * The input array starts out sorted, but will get unsorted as the game goes on,
+       * so binary search is out the window.
+       * Linear search: O(n)
+       */
+      for (int i = 0; i < arr.length; i++) {
+        // TRUE: If we find it, swap the first and last elements
+        if (arr[i] == target) {
+          int temp = arr[0];
+          arr[0] = arr[ARRAY_LENGTH - 1];
+          arr[ARRAY_LENGTH - 1] = temp;
+        }
+        // FALSE: Last element becomes n
+        else {
+          arr[ARRAY_LENGTH - 1] = target;
+        }
       }
-      // FALSE: Last element becomes n
-      else {
-        arr[ARRAY_LENGTH - 1] = target;
+
+      // Calculate the new sum of the array
+      // Linear search: O(n)
+      int sum = 0;
+      for (int i : arr) {
+        sum += i;
       }
+
+      System.out.println("Array sum: " + sum);
+      System.out.print("Guess: ");
+
+      int guess = scanner.nextInt();
+      if (guess == sum) {
+        points++;
+        System.out.println("Correct. You have " + points + " points!");
+      } else {
+        System.out.println("False");
+      }
+
     }
 
-    // Calculate the new sum of the array
-    // Linear search: O(n)
-    int sum = 0;
-    for (int i : arr) {
-      sum += i;
-    }
-
-    System.out.println("Array sum: " + sum);
-    System.out.print("Guess: ");
-
-    int guess = scanner.nextInt();
-    if (guess == sum) {
-      System.out.println("Correct");
-    } else {
-      System.out.println("False");
-    }
 
     scanner.close();
-
+    System.out.println("You win");
     // Total time complexity: O(n)
     // Total space complexity: O(1)
   }
