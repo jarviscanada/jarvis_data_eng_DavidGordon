722df871ca6d51d524e5a050251b90874c284e17
davidgordon12 <davidgordon12@live.ca>
2023-11-10 13:05:50 -0500

    Typescript questions

---
+++ JavaScript_TypeScript/typescript-practice-one/README.md
@@ -0,0 +1,2 @@
+# Questions 1-16 od TypeScript exercises
+https://typescript-exercises.github.io/#exercise=1
---
+++ JavaScript_TypeScript/typescript-practice-one/question_eleven.ts
@@ -0,0 +1,19 @@
+declare module 'str-utils' {
+    // export const ...
+    // export function ...
+    //strReverse,
+    //strToLower,
+    //strToUpper,
+    //strRandomize,
+    //strInvertCase
+
+    function strReverse(str: string): string;
+
+    function strToLower(str: string): string;
+
+    function strToUpper(str: string): string;
+
+    function strRandomize(str: string): string;
+
+    function strInvertCase(str: string): string;
+}
